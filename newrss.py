6a4a7988afac3197e879352bb496bb8c6874997a
oleg <oleg@websplanet.com>
2018-01-29 15:16:03 -0200

    new commit1

--- newrss.py
+++ newrss.py
@@ -13,7 +13,6 @@ from config import *
 #    exit()
 
 #rss_url = sys.argv[1]
-#rss_url = 'https://www.site24x7.com/sv.do?id=TomG9XHaG6PCbxrHL55MhmA5a8ufEbLgD8vvWmukU2LpnofnY%2B9ibvRH9c1%2BorSW4EftR5DVZx7s%0AWMXEoqXfP%2F6PgJrnHQiJ&rss=true'
 print("TITLE","                                                   ","LINK")
 print("------------------------------------------------------------------")
 
@@ -37,8 +36,6 @@ for x in range(0,y):
 
 #############################################################
 
-#TOKEN = "306317133:AAELUg13H13lSOmFiHc_UUmRueJgzy_z8DI"
-#URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
 
@@ -145,8 +142,6 @@ if __name__ == '__main__':
 
 #text, chat = get_last_chat_id_and_text(get_updates())
 #send_message(text, chat)
-#MYTOKEN = "457575840:AAEW_ypqiVRmdEWow05Dqp5Plu27FwWGOEc"
-#MYURL = "https://api.telegram.org/bot{}/".format(MYTOKEN)
 
 #def myget_last_chat_id_and_text(updates):
 #    mynum_updates = len(updates["result"])
